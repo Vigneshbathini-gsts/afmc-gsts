4f7a307853ba1a2bb43ef24e10daa2b443d8a90f
saibethiglobalsparktek <sai.bethi@globalsparktek.com>
2026-05-11 20:43:14 +0530

    backupafmc_mess_11_05_2026

--- server/routes/Pubmenubuyroutes.js
+++ server/routes/Pubmenubuyroutes.js
@@ -5,6 +5,8 @@ const PubmenubuyController = require("../controllers/PubmenubuyController");
 
 router.post("/Pubmenubuy/create", authMiddleware, PubmenubuyController.createPubMenuOrder);
 router.get("/Pubmenubuy/:ORDER_NUMBER", authMiddleware, PubmenubuyController.getPubMenuOrderSummary);
+router.patch("/Pubmenubuy/:ORDER_NUMBER/item/:ITEM_CODE", authMiddleware, PubmenubuyController.updatePubMenuOrderItemQuantity);
+router.delete("/Pubmenubuy/:ORDER_NUMBER/item/:ITEM_CODE", authMiddleware, PubmenubuyController.deletePubMenuOrderItem);
 router.delete("/Pubmenubuy/:ORDER_NUMBER", authMiddleware, PubmenubuyController.cancelPubMenuOrder);
 
 module.exports = router;
