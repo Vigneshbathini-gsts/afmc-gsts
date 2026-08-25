5f95251f7000d4de6722a4a2eba22d32e1d89a5f
ajaykasuri-Gsts <ajaykasuri@gmail.com>
2026-08-25 19:08:37 +0530

    fiexs

--- server/modules/pubmenubuy/Pubmenubuyroutes.js
+++ server/modules/pubmenubuy/Pubmenubuyroutes.js
@@ -14,8 +14,8 @@ router.put(
   checkBarOpen,
   PubmenubuyController.updatePubMenuOrderItemCustomization
 );
-router.delete("/Pubmenubuy/:ORDER_NUMBER/item/:ITEM_CODE", checkBarOpen, PubmenubuyController.deletePubMenuOrderItem);
-router.delete("/Pubmenubuy/:ORDER_NUMBER", checkBarOpen, PubmenubuyController.cancelPubMenuOrder);
+router.delete("/Pubmenubuy/:ORDER_NUMBER/item/:ITEM_CODE", PubmenubuyController.deletePubMenuOrderItem);
+router.delete("/Pubmenubuy/:ORDER_NUMBER", authMiddleware, PubmenubuyController.cancelPubMenuOrder);
 router.put(
   "/Pubmenubuy/:ORDER_NUMBER/line/:ORDER_LINE_ID/quantity",
   checkBarOpen,
